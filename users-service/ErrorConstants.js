918ffa75d6605d3962146ca567a78e85e6375b84
Vit <test@epam.com>
2020-12-17 12:01:55 +0200

    Remove ErrorConstants

--- users-service/ErrorConstants.js
+++
@@ -1,7 +0,0 @@
-const errorConstants = {
-  MONGODB_ERROR: 'mongodb_error',
-  EMAIL_PASSWORD_INVALID: 'email_password_invalid',
-  LOGIN_SUCCESS: 'login_success',
-};
-
-module.exports = Object.freeze(errorConstants);
